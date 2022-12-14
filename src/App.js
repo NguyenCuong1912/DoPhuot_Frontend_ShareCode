848def8f6eaafd3db7373ebe11a0779818ea3e18
luong <luong@example.com>
2022-12-14 21:51:09 +0700

    Admin Account

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react'
 import { createBrowserHistory } from 'history';
 import { Switch, Router } from 'react-router';
 import HomeTemplate from './template/HomeTemplate/HomeTemplate';
-import { _home } from './utils/Utils/ConfigPath';
+import { _account, _add, _admin, _edit, _home, _login, _register } from './utils/Utils/ConfigPath';
 import Home from './pages/Client/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import AdminTemplate from './template/AdminTemplate/AdminTemplate';
+import QuanLyAccount from './pages/Admin/Account/QuanLyAccount';
+import ThemAccount from './pages/Admin/Account/ThemAccount';
+import SuaAccount from './pages/Admin/Account/SuaAccount';
 
 
 
@@ -14,6 +20,15 @@ export default function App() {
     <Router history={history}>
       <Switch>
         <HomeTemplate path={_home} exact Component={Home} />
+        <HomeTemplate path={_login} exact Component={Login} />
+        <HomeTemplate path={_register} exact Component={Register} />
+
+
+        <AdminTemplate path={`${_admin}${_account}`} exact Component={QuanLyAccount} />
+        <AdminTemplate path={`${_admin}${_account}${_add}`} exact Component={ThemAccount} />
+        <AdminTemplate path={`${_admin}${_account}${_edit}/:id`} exact Component={SuaAccount} />
+
+
       </Switch>
     </Router>
   )
--- src/template/HomeTemplate/Header/Header.js
+++ src/template/HomeTemplate/Header/Header.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { _logo } from '../../../utils/Utils/ImgPath'
-import { BsBasket, BsSearch } from "react-icons/bs";
+import { BsBasket, BsPersonCircle, BsSearch } from "react-icons/bs";
 import { NavLink } from 'react-router-dom';
 import NavHeader from './NavHeader';
+import { _account, _home, _login, _order, _register } from '../../../utils/Utils/ConfigPath';
+import { useSelector } from 'react-redux';
+import { Dropdown, Menu, Space } from 'antd';
+import { history } from '../../../App';
+import { USER_LOGIN } from '../../../redux/Types/QuanLyAccountType';
+import _ from 'lodash'
+
 
 export default function Header() {
+    const { userLogin } = useSelector(state => state.QuanLyAccountReducer);
+
+    const menu = (
+        <Menu
+            items={[
+                {
+                    label: <button onClick={() => {
+                        history.push(`${_account}`);
+                        window.location.reload();
+                    }} className="self-center px-4 py-2 hover:font-bold hover:text-yellow-500">Thông tin tài khoản</button>,
+                    key: '0',
+                },
+                {
+                    label: <Fragment>
+                        <button onClick={() => {
+                            history.push(`${_order}/${userLogin.account.id}`);
+                            window.location.reload();
+                        }} className="self-center px-4 py-2 hover:font-bold hover:text-yellow-500">Lịch sử đặt hàng</button>
+                    </Fragment>,
+                    key: '1',
+                },
+                {
+                    label: <button onClick={() => {
+                        sessionStorage.removeItem(USER_LOGIN);
+                        history.push(`${_home}`);
+                        window.location.reload();
+                    }} className="self-center px-4 py-2 hover:font-bold hover:text-yellow-500">Đăng xuất</button>,
+                    key: '2',
+                },
+            ]}
+        />
+    )
+
+
+    const operations = <Fragment>
+        {!_.isEmpty(userLogin) ?
+            <Dropdown
+                overlay={menu}
+                trigger={['click']}
+            >
+                <span onClick={(e) => e.preventDefault()}>
+                    <Space>
+                        <div className='flex items-center text-black mt-2 cursor-pointer hover:text-yellow-400'>
+                            <BsPersonCircle className='text-2xl mr-1' />
+                            <span className='flex items-center text-base font-bold '>{userLogin.account.Username}</span>
+                        </div>
+                    </Space>
+                </span>
+            </Dropdown>
+            : <Fragment>
+                <NavLink to={_login} className='border text-black py-2 px-4 mx-2 rounded-lg hover:text-yellow-500 hover:border-yellow-500 '>Đăng nhập</NavLink>
+                <NavLink to={_register} className='border text-black py-2 px-4 mx-2 rounded-lg hover:text-yellow-500  hover:border-yellow-500 '>Đăng ký</NavLink>
+            </Fragment>}
+    </Fragment>
     return (
         <div className='fixed top-0 w-full z-50 bg-white'>
             <div className='grid grid-cols-12'>
@@ -23,8 +84,7 @@ export default function Header() {
                         </div>
                         <div className='col-span-3 flex justify-end items-center'>
                             <div className='flex items-center uppercase'>
-                                <NavLink to='' className='border text-black py-2 px-4 mx-2 rounded-lg hover:text-yellow-500 hover:border-yellow-500 '>Đăng nhập</NavLink>
-                                <NavLink to='' className='border text-black py-2 px-4 mx-2 rounded-lg hover:text-yellow-500  hover:border-yellow-500 '>Đăng ký</NavLink>
+                                {operations}
                                 <NavLink to='' className='ml-4 text-black text-2xl hover:text-yellow-500'><BsBasket /></NavLink>
                             </div>
                         </div>
